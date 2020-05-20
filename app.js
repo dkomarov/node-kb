d561a5d4544df2e27af30e994cef97e784ee541a
dkomarov <dkomarov@hawk.iit.edu>
2020-05-20 17:37:56 -0500

    configure add route & render article view

--- app.js
+++ app.js
@@ -1,12 +1,24 @@
 const express = require('express')
+const path = require('path');
 
 // Init app
 const app = express();
+
+// Load View Engine
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'pug')
 
 // Home Route
 app.get('/', function(req, res) {
   res.send('Hello world');
 });
+
+// Add Route
+app.get('/articles/add', function(req, res){
+  res.render('add_article', {
+    title: 'Add article'
+  });
+})
 
 // Start Server
 app.listen(3000, function() {
